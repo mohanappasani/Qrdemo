64c058770e249bb4fa0e0e1299663f9e0b51494d
Mohan <mohanappasani@gmail.com>
2022-03-29 17:11:22 +0530

    final

---
+++ src/main/java/com/example/demo/QrdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QrdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QrdemoApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/demo/cofig/WebConfig.java
@@ -0,0 +1,27 @@
+package com.example.demo.cofig;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler(
+                "/webjars/**",
+                "/img/**",
+                "/css/**",
+                "/js/**")
+                .addResourceLocations(
+                        "classpath:/META-INF/resources/webjars/",
+                        "classpath:/static/img/",
+                        "classpath:/static/css/",
+                        "classpath:/static/js/");
+    }
+
+}
+
